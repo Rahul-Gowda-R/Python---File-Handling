adb432e6b8a080de48ebd6b358dacd0f8543dd38
Rahul Gowda R <rrahulgowda733@gmail.com>
2026-02-09 17:32:29 +0530

    Add files via upload

--- task3.py
+++ task3.py
@@ -4,6 +4,50 @@
 #2 fn_dno to transform dno to deptname
 #3 capitalize the name
 #generate the final list as per sample:
-#[(101,'amar',50000,male,admin),.......]
+#[(101,'Amar',50000,male,admin),.......]
 #after this find the number employees in each dept name
 #find total salary based dept name
+
+
+def fn_gender(g):
+    if g=='M' or g=='m':
+        return "Male"
+    elif g=='F' or g=='f':
+        return "Female"
+    
+def fn_dno(dno):
+    dno=int(dno)
+
+    if dno==11:
+        return "Admin"
+    elif dno==12:
+        return "HR"
+    elif dno==13:
+        return "IT"
+    elif dno==14:
+        return "Finance"
+    elif dno==15:
+        return "Sales"
+    else:
+        return "No such department"
+
+    
+file=open('emp.txt')
+
+file.readline()
+
+
+for line in file:
+    data=line.strip().split(",")
+
+    data[1]=data[1].capitalize()
+
+    data[3]= fn_gender(data[3])
+
+    data[4]= fn_dno(data[4])
+
+    print(data)
+
+    
+
+file.close()
