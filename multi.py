ecffeb7b85db471684e34d76d7b53248d0722a4c
Rahul Gowda R <rrahulgowda733@gmail.com>
2026-02-12 17:32:16 +0530

    Add files via upload

---
+++ multi.py
@@ -0,0 +1,18 @@
+#working with multiple files
+
+#master file and transaction file
+#master file and detail file
+#parent file and child file
+
+#master file will have unique data with id's 
+#trasnaction file uses the data of master file
+
+#ex.master file:101,Amar
+#in trans file:
+    #101,2000
+    #101,2000
+
+#whwn workingf with multiple files all files should have at least one common column
+
+#1.read all master file and create dictionary
+
